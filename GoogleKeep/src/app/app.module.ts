4f76d9bb943f5c4252daa524e371038e5555b4df
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-08-21 09:34:57 +0530

    done services

---
+++ GoogleKeep/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotesComponent,
+    NoteDetailComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ GoogleKeep/src/app/note.ts
@@ -0,0 +1,4 @@
+export class Note{
+  id: number;
+  header: string;
+}
